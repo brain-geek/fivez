0ef2076c2b9690fac8156ce2ccd1c73465c4cf32
Alex Rozumey <brain-geek@yandex.ua>
2015-08-30 17:21:34 +0300

    Added working UI with arrow key controls working

--- cli_ui/base.go
+++ cli_ui/base.go
@@ -9,9 +9,33 @@ func StartGameUI() {
 	game := core.NewGame()
 
 	draw(game)
+
+mainloop:
+	for {
+		_, char, _ := getChar()
+
+		fmt.Println(char)
+
+		switch char {
+		case 0: //KeyEsc:
+			break mainloop
+		case 37: //KeyArrowLeft:
+			game.Move(core.LEFT)
+		case 39: //KeyArrowRight:
+			game.Move(core.RIGHT)
+		case 38: //KeyArrowUp:
+			game.Move(core.UP)
+		case 40: //KeyArrowDown:
+			game.Move(core.DOWN)
+		default:
+		}
+		draw(game)
+	}
 }
 
 func draw(game *core.Game) {
+	fmt.Print("\033[H\033[2J")
+
 	fmt.Println("/=======================\\")
 
 	for i, row := range game.Positions() {

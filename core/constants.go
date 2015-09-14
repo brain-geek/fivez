5846bda4887b127cd1c4168fa2948fe585e735bb
Alex Rozumey <brain-geek@yandex.ua>
2015-09-14 20:52:41 +0300

    Extracted constants in core to separate file

---
+++ core/constants.go
@@ -0,0 +1,21 @@
+package core
+
+const (
+	// Game field is 4x4
+	GameFieldSize = 4
+
+	// Where to start the first block
+	FirstBlockX = 1
+	FirstBlockY = 2
+
+	// What will be in the first block
+	FirstBlockData = 2
+
+	// Starting from what number we allow only self-merges
+	FirstHardNumber = 5
+
+	UP    = 0
+	LEFT  = 1
+	DOWN  = 2
+	RIGHT = 4
+)
